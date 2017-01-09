c4d748abe2239b18dd842ce36e4dffc8b47fcf6b
Alex Cauchi <alex_cauchi@hotmail.com>
2017-01-09 20:49:22 +0100

    added sql managment class and methods

---
+++ ContentManagmentSystem.Test/SQLManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.SqlClient;
+using System.IO;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+
+namespace ContentManagmentSystem
+{
+    public class SQLManager
+    {
+
+        public void ResetDatabase()
+        {
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+
+            FileInfo file = new FileInfo("../../Scripts/Users.sql");
+            string script = file.OpenText().ReadToEnd();
+            string resetIdScript = "DBCC CHECKIDENT('Users', RESEED, 0); ";
+
+            SqlConnection cnn = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand();    
+            cmd.Connection = cnn;
+            cmd.CommandTimeout = 1000;
+            cmd.CommandText = "DELETE From Users;"+ resetIdScript + script;
+            cnn.Open();
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+         }
+
+    }
+}
--- ContentManagmentSystem.Test/UnitTest1.cs
+++ ContentManagmentSystem.Test/UnitTest1.cs
@@ -8,10 +8,64 @@ namespace ContentManagmentSystem.Test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void AuthenticateUserCorrectCredentials()
         {
             UserManagement um = new UserManagement();
-            um.AddNewUser("nik", "nik@n.n", "123456", "admin");
+            var isAuthenticated = um.Authenticate("ssimmons0", "MtqOJc");
+            Assert.IsTrue(isAuthenticated);
+
         }
+        [TestMethod]
+        public void AuthenticateUserInCorrectCredentials()
+        {
+            UserManagement um = new UserManagement();
+            var isAuthenticated = um.Authenticate("ssimmons", "MtqO");
+            Assert.IsFalse(isAuthenticated);
+        }
+        [TestMethod]
+        public void GetRoleByUsername()
+        {
+            UserManagement um = new UserManagement();
+            var role = um.GetRoleByUsername("ssimmons0");
+            Assert.IsNotNull(role);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(UserNotFoundException))]
+        public void GetRoleByIncorrectUsername()
+        {
+            string userRole = "";
+            try
+            {
+                UserManagement um = new UserManagement();
+                userRole = um.GetRoleByUsername("nikola");
+            }
+            finally
+            {
+                Assert.IsNull(userRole);
+            }
+        }
+        [TestMethod]
+        public void AddNewUser()
+        {
+            UserManagement um = new UserManagement();
+            var user = um.AddNewUser("nikola", "nik@a.a", "123456", "Learner");
+            Assert.IsNotNull(user);
+        }
+
+        [TestMethod]
+       /// [ExpectedException(typeof(ArgumentException))]
+        public void UpdateUserPassword()
+        {
+            UserManagement um = new UserManagement();
+            um.UpdateUserPassword("ssins0", "ssimmons0");
+        }
+
+        [TestCleanup]
+        public void Finalise() {
+            SQLManager sqlManager = new SQLManager();
+            sqlManager.ResetDatabase();
+        }
+
+
     }
 }
